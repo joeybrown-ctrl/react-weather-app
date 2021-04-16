dee6366b7e34788918400c99fee769d8e783f50f
Joey Brown <gjoey.brown@gmail.com>
2021-04-16 13:44:25 -0700

    Set up promises within app.js. Got data showing in console.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,34 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  //then I set two states: one for lat and one for long
+  //then I set three states: one for lat, one for long, and one to set the state for data.
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-
-  //then I create the useEffect function
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+  const [data, setData] = useState([]);
 
     //lat and long are gotten using navigator.geolocation.
     //setLat and setLong are used to set the states for latitude and longitude.
 
-    console.log("Latitude is:", lat);
-    console.log("Longitude is:", long);
+  //then I create the useEffect function
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+
+  fetchData();
+
   }, [lat, long]);
+
 
   return (
     <div className="App">
