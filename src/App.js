14b0f612cff1fb22df1f69827e96b74d7f550a44
Joey Brown <gjoey.brown@gmail.com>
2021-04-16 13:08:02 -0700

    Imported react hooks and created functionality for lat and long within app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
+//importing necessary hooks from react
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  //then I set two states: one for lat and one for long
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  //then I create the useEffect function
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    //lat and long are gotten using navigator.geolocation.
+    //setLat and setLong are used to set the states for latitude and longitude.
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }, [lat, long]);
+
   return (
     <div className="App">
     </div>
