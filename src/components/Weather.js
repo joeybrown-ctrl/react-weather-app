e5c528f9f088c4de81dcdeed5689b8444bd12308
Joey Brown <gjoey.brown@gmail.com>
2021-04-19 12:49:51 -0700

    fixed buggy moment.js block of code within weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,34 +1,31 @@
 import moment from 'moment';
 import React from 'react';
-import { Card } from 'semantic-ui-react';
-// import './styles.css';
+import './styles.css';
 
 const WeatherCard = ({weatherData}) => {
     return (
-    <Card>
-        <Card.Content>
-            <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
-            <p>Temperature: {weatherData.main.temp} &deg;C</p>
-            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Description: {weatherData.weather[0].main}</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-            <p>Day: {moment().format('dddd')}</p>
-            <p>Date: {moment().format('LL')}</p>
-        </Card.Content>
-    </Card>
+
+        <div className="main">
+            <p className="header">{weatherData.name}</p>
+            <div className="flex">
+                <p className="day">{moment().format('dddd')}</p>
+                <p className="day">{moment().format('LL')}</p>
+                <p className="description">{weatherData.weather[0].main}</p>
+            </div>
+            
+            <div className="flex">
+                <p className="temp">Temperature: {weatherData.main.temp} &deg;C</p>
+                <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+            </div>
+
+            <div className="flex">
+                <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+                <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            </div>
+        </div>
     )
 }
 
 export default WeatherCard;
 
-{/* <Card.Content>
-<Card.Header className="header">City Name: {weatherData.name}</Card.Header>
-<p>Temprature: {weatherData.main.temp} &deg;C</p>
-<p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-<p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-<p>Description: {weatherData.weather[0].main}</p>
-<p>Humidity: {weatherData.main.humidity} %</p>
-<p>Day: {moment().format('dddd')}</p>
-<p>Date: {moment().format('LL')}</p>
-</Card.Content> */}
+
