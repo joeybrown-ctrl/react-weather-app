f5d520a0acc556723c44b84bf3e100e5d3ff7015
Joey Brown <gjoey.brown@gmail.com>
2021-04-19 12:13:02 -0700

    got data rendering to card component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 // import './styles.css';
@@ -6,10 +7,28 @@ const WeatherCard = ({weatherData}) => {
     return (
     <Card>
         <Card.Content>
-            <Card.Header className="header">{weatherData.name}</Card.Header>
+            <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
+            <p>Temperature: {weatherData.main.temp} &deg;C</p>
+            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            <p>Description: {weatherData.weather[0].main}</p>
+            <p>Humidity: {weatherData.main.humidity}%</p>
+            <p>Day: {moment().format('dddd')}</p>
+            <p>Date: {moment().format('LL')}</p>
         </Card.Content>
     </Card>
     )
 }
 
 export default WeatherCard;
+
+{/* <Card.Content>
+<Card.Header className="header">City Name: {weatherData.name}</Card.Header>
+<p>Temprature: {weatherData.main.temp} &deg;C</p>
+<p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+<p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+<p>Description: {weatherData.weather[0].main}</p>
+<p>Humidity: {weatherData.main.humidity} %</p>
+<p>Day: {moment().format('dddd')}</p>
+<p>Date: {moment().format('LL')}</p>
+</Card.Content> */}
